57ee2e3d2577ae04e60e09afc56a4c1435521ac6
Egor Pyntikov <egorpyntik@gmail.com>
2024-02-05 19:02:45 +0300

    hw_15

---
+++ index.js
@@ -0,0 +1,51 @@
+
+//основное задание
+/* var firstNumber = Number(prompt('Введите 1 число'));
+var secondNumber = Number(prompt('Введите 2 число'));
+var thirdNumber = Number(prompt('Введите 3 число'));
+var result = prompt('Числа сложить или умножить?');
+if (result == 'сложить') {
+    alert(`Сумма равна ${firstNumber + secondNumber + thirdNumber}`);
+} else if (result == 'умножить') {
+    alert(`Произведение равно ${firstNumber * secondNumber * thirdNumber}`);
+} else {
+    alert('Дан невреный ответ на вопрос');
+} */
+
+//задание со *
+var firstNumber = Number(prompt('Введите 1 число'));
+var secondNumber = Number(prompt('Введите 2 число'));
+var thirdNumber = Number(prompt('Введите 3 число'));
+
+if (firstNumber == '') {
+    firstNumber = Number(prompt('Введите 1 число еще раз'));
+}
+
+if (secondNumber == '') {
+    secondNumber = Number(prompt('Введите 2 число еще раз'));
+}
+
+if (thirdNumber == '') {
+    thirdNumber = Number(prompt('Введите 3 число еще раз'));
+}
+
+if (firstNumber, secondNumber, thirdNumber == '') {
+    alert('Пока');
+}
+
+
+if (firstNumber, secondNumber, thirdNumber == null) {
+    var text = prompt('Почему вы отменили?');
+}
+if (text) {
+    alert(`Я понял вашу причину "${text}", всего доброго...`)
+}
+if (firstNumber == NaN) {
+    alert('Были введены некорректные данные')
+}
+if (secondNumber == NaN) {
+    alert('Были введены некорректные данные')
+}
+if (thirdNumber == NaN) {
+    alert('Были введены некорректные данные')
+}
